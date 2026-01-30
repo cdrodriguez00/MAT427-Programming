53b59f6faeb1ffb0e1e4b6a4b3b1e457a54cbfa4
cristianrodriguez <cristiandrodriguez74@outlook.com>
2026-01-30 00:38:11 -0500

    first commit

--- Programming 1-1.py
+++ Programming 1-1.py
@@ -11,7 +11,7 @@ import math
 
 ''' compute e^-x using taylor's polynomials '''
 def taylor1(x, n):
-    xvals = [-50, -20, -15, 10, -5 , -1, 1, 5,10, 50, 100, 500, 1000]
+    xvals = [-50, -20, -15, 10, -5, -1, 1, 5,10, 50, 100, 500, 1000]
     ndegree = [1, 5, 10, 50, 100]
 
 
