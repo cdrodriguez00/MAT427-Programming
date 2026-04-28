3dcb4731bc98bf27a4716bf60754cf84a77158a5
cristianrodriguez <cristiandrodriguez74@outlook.com>
2026-04-28 13:41:23 -0400

    commit

--- Programming 2-1.py
+++ Programming 2-1.py
@@ -75,6 +75,8 @@ print("1. The maximum error using N=1000 for the standard evaluation function is
 print("2. The maximum error using N =1000 for the Horner's evaluation function is", horner_polynomial_error)
 
 N = 100000
+standard_polynomial_error = maximum_error(p, a, b, N, standard_polynomial)
+horner_polynomial_error = maximum_error(p, a, b, N, horner)
 
 print("1. The maximum error using N=100000 for the standard evaluation function is", standard_polynomial_error)
 print("2. The maximum error using N =100000 for the Horner's evaluation function is", horner_polynomial_error)
