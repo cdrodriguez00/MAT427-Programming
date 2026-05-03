a114a8365412e1aab3a14d1e3685ffbb0e065b29
cristianrodriguez <cristiandrodriguez74@outlook.com>
2026-05-03 02:54:16 -0400

    commit

--- Programming 2-2.py
+++ Programming 2-2.py
@@ -6,6 +6,7 @@ Assignment: Chapter 2.2
 Difference Quotients
 """
 
+import math
 
 def forward_diff_quotient(f, x, h):
     return (f(x+h) - f(x))/ h
@@ -22,6 +23,15 @@ def better_foward_diff_quotient(f, x, h):
 def derivative_approx (f,x, h):
     2*forward_diff_quotient(f,x,h) - better_foward_diff_quotient(f,x,h)
 
+def convergence(errors):
+    calculations = [None]
+
+    for i in range(1, len(errors)):
+
+        calculation = math.log(better_foward_diff_quotient() / forward_diff_quotient()) / math.log(2)
+        calculations.append(calculation)
+
+    return calculations
 
 
 
