87e23571ff22e0f2a9462fabb7341de8e64ed63d
cristianrodriguez <cristiandrodriguez74@outlook.com>
2026-05-03 00:41:13 -0400

    commit

--- Programming 2-2.py
+++ Programming 2-2.py
@@ -7,3 +7,11 @@ Difference Quotients
 """
 
 
+def right_hand_diff_quotient(f, x, h):
+    return (f(x+h) - f(x))/ h
+
+def left_hand_diff_quotient(f, x, h):
+    return (f(x) - f(x-h)) / h
+
+def central_diff_quotient(f, x, h):
+    return (f(x+h)-f(x-h)) / h
